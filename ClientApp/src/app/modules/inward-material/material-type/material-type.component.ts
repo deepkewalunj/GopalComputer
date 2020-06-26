2f980dadb07bc8756ca8d3b4e8c0916f8d792534
nitinpatil <nitinpatil583@gmail.com>
2020-06-26 17:25:00 +0530

    material type

--- ClientApp/src/app/modules/inward-material/material-type/material-type.component.ts
+++ ClientApp/src/app/modules/inward-material/material-type/material-type.component.ts
@@ -1,4 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup,  FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-material-type',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MaterialTypeComponent implements OnInit {
 
-  constructor() { }
+  searchFilter: boolean;
 
-  ngOnInit() {
+  private _success = new Subject<string>();
+  staticAlertClosed = false;
+  successMessage: string;
+
+    // we used reactive forms and validations
+    addMaterialTypeForm: FormGroup;
+    constructor(private fb: FormBuilder, private modalService: NgbModal) {
+     this.createForm();
+    }
+  
+    createForm() { 
+      this.addMaterialTypeForm = this.fb.group({
+        title:      ['', Validators.required],
+      personName: ['', Validators.required],
+      companyName:          ['', Validators.required],
+      phoneNo: ['', Validators.required],
+      },
+     
+      );
+    }
+
+
+  /*on click modal will be open*/
+
+  open(content) {
+    this.modalService.open(content);
+  }
+
+  addClientPopup(content) {
+    this.modalService.open(content, { size: 'lg' });
+  }
+
+   /*succes message code here*/
+ 
+  ngOnInit(): void {
+    setTimeout(() => this.staticAlertClosed = true, 20000);
+
+    this._success.subscribe((message) => this.successMessage = message);
+    this._success.pipe(
+      debounceTime(5000)
+    ).subscribe(() => this.successMessage = null);
+  }
+
+  public changeSuccessMessage() {
+    this._success.next('Record deleted successfully.');
+  }
+
+  /*on click search filter hide show on mobile*/
+
+  toggleSearch() {
+    this.searchFilter = !this.searchFilter;    
   }
 
 }
+
